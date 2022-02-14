9576c5e3704a36d79978eae5b63f40ab789ec566
James Truong <jtruong123@outlook.com>
2022-02-14 14:27:38 -0800

    Added various AI improvements
    * Better prompt context
    * Retain previous responses
    * Adjusted frequency penalty

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,31 @@
+let currentPrompt = getOriginalPrompt();
+let responseCount = 0;
+
 let formEl = document.getElementById('form');
 
 formEl.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    let reponse = axios.post(
+    let message = e.target.message.value;
+    console.log(message);
+
+    // After n responses, the prompt's context will reset to prevent the conversation from getting stuck in a loop.
+    if (responseCount > 3) {
+        currentPrompt = getOriginalPrompt();
+    }
+
+    // Append our message to the prompt.
+    currentPrompt += message;
+
+    let response = axios.post(
         'https://api.openai.com/v1/engines/text-davinci-001/completions',
         {
-            prompt: "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: " + e.target.prompt.value,
+            // https://beta.openai.com/docs/api-reference/completions/create
+            prompt: currentPrompt,
             temperature: 0.9,
             max_tokens: 150,
             top_p: 1,
-            frequency_penalty: 0,
+            frequency_penalty: 1.0,
             presence_penalty: 0.6,
             stop: [" Human:", " AI:"]
         },
@@ -22,9 +37,27 @@ formEl.addEventListener('submit', (e) => {
         },
     );
 
-    reponse.then(response => {
-        console.log(response.data);
+    response.then(result => {
+        let response = result.data.choices[0].text;
+
+        currentPrompt += response;
+        currentPrompt += '\nHuman: ';
+
+        console.log('Message: ' + message);
+        console.log('Response: ' + response);
+        console.log('Prompt: ' + currentPrompt);
+
+        let responsesEl = document.getElementById('responses');
+
+        let responseEl = document.createElement('p');
+        responseEl.innerText = response;
+
+        responsesEl.appendChild(responseEl);
     }).catch(error => {
         console.log(error);
     });
 });
+
+function getOriginalPrompt() {
+    return 'The following is a conversation with an AI assistant named "V" that provides dating and relationship advice. The assistant is helpful, understanding, and very friendly. Today is Valentine\'s day\n\nV: How can I help you today?\nHuman: ';
+}
